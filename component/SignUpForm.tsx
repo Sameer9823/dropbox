bbc8ce124fd3016c05c249d7cabdc6f74cf1d772
Sameer Selokar <sameerselokar924@gmail.com>
2025-05-08 15:34:11 +0530

    feat: implement sign-in schema validation using zod

---
+++ component/SignUpForm.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+import  {useForm} from "react-hook-form"
+import {useSignUp} from "@clerk/nextjs"
+import { z } from "zod"
+import { signUpSchema } from "@/schemas/signUpSchema"
+
+
+export default function SignUpForm() {
+    const onSubmit = async () => {
+
+    }
+    const handleVerificationSubmit = async () => {
+        
+    }
+}
