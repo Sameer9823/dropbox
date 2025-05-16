6d8094a65ae9491e4e233168ff25849e4d5cedf6
Sameer Selokar <sameerselokar924@gmail.com>
2025-05-16 16:44:10 +0530

    feat: implement POST route for file uploads with parent folder validation

---
+++ app/api/files/upload/route.ts
@@ -0,0 +1,107 @@
+import { db } from "@/lib/db";
+import { files } from "@/lib/db/schema";
+import { auth } from "@clerk/nextjs/server";
+import { eq, and } from "drizzle-orm";
+import { v4 as uuidv4 } from "uuid";
+import ImageKit from "imagekit";
+import { NextResponse } from "next/server";
+
+const imagekit = new ImageKit({
+  publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY || "",
+  privateKey: process.env.IMAGEKIT_PRIVATE_KEY || "",
+  urlEndpoint: process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT || "",
+});
+
+export async function POST(request: Request) {
+  try {
+    const { userId } = await auth();
+    if (!userId) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    const formData = await request.formData();
+    const file = formData.get("file") as File;
+    const formUserId = formData.get("userId") as string;
+    const parentId = (formData.get("parentId") as string) || null;
+
+    if (formUserId !== userId) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    if (!file) {
+      return NextResponse.json({ error: "File is required" }, { status: 400 });
+    }
+
+    if (parentId) {
+      const [parentFolder] = await db
+        .select()
+        .from(files)
+        .where(
+          and(
+            eq(files.id, parentId),
+            eq(files.userId, userId),
+            eq(files.isFolder, true)
+          )
+        );
+    } else {
+      return NextResponse.json(
+        { error: "Parent folder is required" },
+        { status: 400 }
+      );
+    }
+
+    if (!parentId) {
+      return NextResponse.json(
+        { error: "Parent folder is required" },
+        { status: 400 }
+      );
+    }
+
+    if (!file.type.startsWith("image/") && file.type !== "application/pdf") {
+      return NextResponse.json({ error: "Invalid file type" }, { status: 400 });
+    }
+
+    const buffer = await file.arrayBuffer();
+    const fileBuffer = Buffer.from(buffer);
+
+
+    const folderPath = parentId ? `/droply/${userId}/folder/${parentId}` : `/droply/${userId}`;
+
+    const originalFilename = file.name;
+    const fileExtension = originalFilename.split(".").pop() || "";
+    //check empty extension
+  if(!fileExtension){
+    return NextResponse.json({ error: "Invalid file type" }, { status: 400 });
+  }
+
+    const uniqueFilename = `${uuidv4()}.${fileExtension}`;
+
+   const uploadResponse =  await imagekit.upload({
+      file: fileBuffer,
+      fileName: uniqueFilename,
+      folder: folderPath,
+      useUniqueFileName: false,
+    });
+
+    const fileData = {
+      name: originalFilename,
+      path: uploadResponse.filePath,
+      size: file.size,
+      type: file.type,
+      fileUrl: uploadResponse.url,
+      thumbnailUrl: uploadResponse.thumbnailUrl || null ,
+      userId: userId,
+      parentId: parentId,
+      isFolder: false,
+      isStarred: false,
+      isTrashed: false
+    };
+
+   const [newFile] = await db.insert(files).values(fileData).returning()
+
+   return NextResponse.json(newFile)
+    
+  } catch (error) {
+    return NextResponse.json({ error: "Failed to upload file" }, { status: 500 });
+  }
+}
